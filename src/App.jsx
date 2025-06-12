3fd96b02e70e63180dfcd969d9d443dfff5d14b9
Lucas_Sartori <lucassartori0604@gmail.com>
2025-06-12 13:04:58 -0300

    atualiza BrowserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import { Routes, Route } from "react-router-dom";
 function App() {
   return (
     <>
-      <BrowserRouter>
+      <BrowserRouter basename="/Portfolio">
         <Routes>
           <Route index element={<Home />} />
 
